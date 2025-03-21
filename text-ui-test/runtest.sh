a0a3877085716f4715f8ee13af73b705f66be9a2
NotGiggs <giggslohjayhon2002@gmail.com>
2025-03-21 23:49:03 +0800

    Update text-ui-test

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# change to script directory
+# Change to script directory
 cd "${0%/*}"
 
 cd ..
@@ -8,11 +8,23 @@ cd ..
 
 cd text-ui-test
 
-java  -jar $(find ../build/libs/ -mindepth 1 -print -quit) < input.txt > ACTUAL.TXT
+# Get today's date in YYYY-MM-DD format
+TODAY=$(date +%Y-%m-%d)
 
-cp EXPECTED.TXT EXPECTED-UNIX.TXT
+# Replace <DATE> with actual date and save to EXPECTED-UNIX.TXT
+sed -E "s/<DATE>/$TODAY/g" EXPECTED.TXT > EXPECTED-UNIX.TXT
+
+# Ensure both files have Unix line endings
 dos2unix EXPECTED-UNIX.TXT ACTUAL.TXT
-diff EXPECTED-UNIX.TXT ACTUAL.TXT
+
+# Find the latest jar file
+JAR_FILE=$(ls -t ../build/libs/*.jar | head -n 1)
+
+# Run the test
+java -jar "$JAR_FILE" < input.txt > ACTUAL.TXT
+
+# Compare output
+diff -wB EXPECTED-UNIX.TXT ACTUAL.TXT
 if [ $? -eq 0 ]
 then
     echo "Test passed!"
